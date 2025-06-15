ec41aa1961b7ced3c44ace2ca0747a5b22b6e8b0
Kawan <kawan-silva@outlook.com>
2025-06-15 19:47:11 -0300

    criar IUserRepository

--- src/main/java/br/com/kawan/todolist/user/UserController.java
+++ src/main/java/br/com/kawan/todolist/user/UserController.java
@@ -2,14 +2,20 @@ package br.com.kawan.todolist.user;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUsername());
+    public UserModel create(@RequestBody UserModel userModel) {
+      var userCreated =  this.userRepository.save(userModel);
+
+      return userCreated;
     }   
 }
