c4afe7890270750351ce792fdb0272bf56e54356
Kawan <kawan-silva@outlook.com>
2025-06-18 23:16:05 -0300

    atualizar parcialmente uma task

--- src/main/java/br/com/kawan/todolist/task/TaskController.java
+++ src/main/java/br/com/kawan/todolist/task/TaskController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.kawan.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -54,9 +55,10 @@ public class TaskController {
 
     @PutMapping("/{id}")
     public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id){
-        var idUser = request.getAttribute("idUser");
-        taskModel.setIdUser((UUID) idUser);
-        taskModel.setId(id);
-        return this.taskRepository.save(taskModel);
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        Utils.copyNonNUllProperties(taskModel, task);
+
+        return this.taskRepository.save(task);
     }
 }
